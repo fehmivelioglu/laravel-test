275613cd2bc5e0b96bebb67f55a04a7ed7ac3fdc
fehmi <fehmi_ali@hotmail.com>
2025-04-11 10:58:34 +0300

    JobController added for route group

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Job;
+
+class JobController extends Controller
+{
+    public $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Laravel Developer',
+            'location' => 'İstanbul',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Flutter Developer',
+            'location' => 'Ankara',
+        ],
+        [
+            'id' => 3,
+            'title' => 'React Developer',
+            'location' => 'İzmir',
+        ],
+    ];
+    public function index()
+    {
+        $jobs = Job::with('employer')->latest()->paginate(5);
+        return view('jobs', [
+            'title' => 'İş İlanları',
+            'description' => 'Mevcut iş ilanları listesi',
+            'jobs' => $jobs
+        ]);
+    }
+
+    public function createPage()
+    {
+        return view('jobs.create');
+    }
+
+    public function show($id)
+    {
+        $job = collect($this->jobs)->firstWhere('id', $id);
+
+        if (!$job) {
+            abort(404, 'İş ilanı bulunamadı.');
+        }
+
+        return view('job', [
+            'job' => $job,
+        ]);
+    }
+
+    public function create()
+    {
+        // dd(request()->all());
+
+        request()->validate([
+            'title' => ['required', 'min:3', 'max:255'],
+            'location' => ['required', 'min:3', 'max:255'],
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'location' => request('location'),
+            'employer_id' => 1,
+        ]);
+
+        return redirect('/jobs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,11 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-$jobs = [
-    [
-        'id' => 1,
-        'title' => 'Laravel Developer',
-        'location' => 'İstanbul',
-    ],
-    [
-        'id' => 2,
-        'title' => 'Flutter Developer',
-        'location' => 'Ankara',
-    ],
-    [
-        'id' => 3,
-        'title' => 'React Developer',
-        'location' => 'İzmir',
-    ],
-];
-
 // * dd -> die and dump
-Route::get('/', function () {
+Route::get('/index', function () {
     return view('index', [
         'title' => 'Ana Sayfa',
         'description' => 'Bu sayfa ana sayfasıdır.',
@@ -52,44 +35,16 @@ Route::get('/greeting', function () {
         ->with('occupation', 'Astronaut');
 });
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->paginate(5);
-    return view('jobs', [
-        'title' => 'İş İlanları',
-        'description' => 'Mevcut iş ilanları listesi',
-        'jobs' => $jobs
-    ]);
+// Route::resource('jobs', JobController::class);
+
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'createPage');
+    Route::get('/jobs/{id}', 'show');
+    Route::post('/jobs', 'create');
 });
 
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
-    $job = collect($jobs)->firstWhere('id', $id);
-
-    if (!$job) {
-        abort(404, 'İş ilanı bulunamadı.');
-    }
-
-    return view('job', [
-        'job' => $job,
-    ]);
-});
-
-Route::post('/jobs', function () {
-    // dd(request()->all());
-
-    request()->validate([
-        'title' => ['required', 'min:3', 'max:255'],
-        'location' => ['required', 'min:3', 'max:255'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'location' => request('location'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-});
+// Route::get('/jobs', [JobController::class, 'index']);
+// Route::get('/jobs/create', [JobController::class, 'createPage']);
+// Route::get('/jobs/{id}', [JobController::class, 'show']);
+// Route::post('/jobs', [JobController::class, 'create']);
